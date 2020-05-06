950433240869f8740ffc93a5bc761d353f550adc
allezz <aerbay@gmail.com>
2020-05-06 20:21:25 +0200

    Add LoginForm,Add user_can_login spec,Add authentication module

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,65 @@
 import React, { Component } from "react";
 import Menu from "./components/Menu";
+import LoginForm from "./components/LoginForm";
+import { authenticate } from "./modules/auth";
 
 class App extends Component {
+  state = {
+    renderLoginForm: false,
+    authenticated: false,
+    message: "",
+  };
+
+  onLogin = async (e) => {
+    e.preventDefault();
+    const response = await authenticate(
+      e.target.email.value,
+      e.target.password.value
+    );
+    if (response.authenticated) {
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ message: response.message, renderLoginForm: false });
+    }
+  };
+
+  onChangeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { renderLoginForm, authenticated, message } = this.state;
+    let renderLogin;
+    switch (true) {
+      case renderLoginForm && !authenticated:
+        renderLogin = <LoginForm submitFormHandler={this.onLogin} />;
+        break;
+      case !renderLoginForm && !authenticated:
+        renderLogin = (
+          <>
+            <button
+              id="login"
+              onClick={() => this.setState({ renderLoginForm: true })}
+            >
+              Login
+            </button>
+            <p id="message">{message}</p>
+          </>
+        );
+        break;
+      case authenticated:
+        renderLogin = (
+          <p id="message">
+            Hi {JSON.parse(sessionStorage.getItem("credentials")).uid}
+          </p>
+        );
+        break;
+    }
+
     return (
       <div>
         <Menu />
+        {renderLogin}
       </div>
     );
   }
