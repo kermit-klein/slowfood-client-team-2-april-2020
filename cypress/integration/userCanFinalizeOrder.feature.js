4b1fd96083f086295d427c29f167042fb4964ca4
allezz <aerbay@gmail.com>
2020-05-10 05:17:07 +0200

    Add Finalize order function in menu.jsx,Adjust menu.jsx file to print confirmation,Fix test file

--- cypress/integration/userCanFinalizeOrder.feature.js
+++ cypress/integration/userCanFinalizeOrder.feature.js
@@ -1,4 +1,4 @@
-describe("User can add a product to their order", () => {
+describe("User can add products and finalize order", () => {
   beforeEach(() => {
     cy.server();
     cy.route({
@@ -18,71 +18,10 @@ describe("User can add a product to their order", () => {
       url: "http://localhost:3000/api/v1/orders/1",
       response: "fixture:put_response.json",
     });
-  });
-
-  describe("user can add multiple products", () => {
-    it("user gets confirmation message when adding a product to order", () => {
-      cy.visit("/");
-      cy.get("#menu-item-1").within(() => {
-        cy.get("button").contains("Add to order").click();
-        cy.get(".message").should(
-          "contain",
-          "The product has been added to your order"
-        );
-      });
-      cy.get("#menu-item-2").within(() => {
-        cy.get("button").contains("Add to order").click();
-        cy.get(".message").should(
-          "contain",
-          "The product has been added to your order"
-        );
-      });
-    });
+    cy.visit("/");
   });
 
   describe("user can add multiple products ", () => {
-    it("and view order details", () => {
-      cy.visit("/");
-      cy.get("button").contains("View order").should("not.exist");
-      cy.get("#menu-item-1").within(() => {
-        cy.get("button").contains("Add to order").click();
-        cy.get(".message").should(
-          "contain",
-          "The product has been added to your order"
-        );
-      });
-
-      cy.get("button").contains("View order").should("exist");
-      cy.get("button").contains("View order").click();
-      cy.get("#order-details").within(() => {
-        cy.get("li")
-          .should("have.length", 1)
-          .first()
-          .should("have.text", "1 x Salad");
-      });
-      cy.get("#total-amount").should("contain", "4");
-
-      cy.get("#menu-item-2").within(() => {
-        cy.get("button").contains("Add to order").click();
-        cy.get(".message").should(
-          "contain",
-          "The product has been added to your order"
-        );
-      });
-
-      cy.get("#order-details").within(() => {
-        cy.get("li")
-          .should("have.length", 2)
-          .first()
-          .should("have.text", "1 x Salad")
-          .next()
-          .should("have.text", "1 x Ice Cream");
-      });
-      cy.get("#total-amount").should("contain", "7.75");
-      cy.get("button").contains("View order").click();
-      cy.get("#order-details").should("not.exist");
-    });
-
     it("user can finalize the order", () => {
       cy.get("#menu-item-1").within(() => {
         cy.get("button").contains("Add to order").click();
